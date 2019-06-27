d4b6ef4680df96243931ac5435301ce0f43d4678
Lep Sistemas <lepsistemas@gmail.com>
2019-06-27 12:28:15 -0300

    First commit

---
+++ src/test/java/br/com/lepsistemas/londondiscovery/spring/LondonDiscoveryApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.lepsistemas.londondiscovery.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LondonDiscoveryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
